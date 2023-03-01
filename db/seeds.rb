33cf2f0f5cc9c0a7af4f7ccbf6f509eb75e44aaa
Mauricio Mendoza <rbreva@gmail.com>
2023-03-01 11:53:50 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Message.destroy_all
+
+Message.create!([
+  {
+    msg: "Primer Mensaje"
+  },
+  {
+    msg: "Segundo Mensaje"
+  },
+  {
+    msg: "Tercer Mensaje"
+  },
+  {
+    msg: "Cuarto Mensaje"
+  },
+  {
+    msg: "Quinto Mensaje"
+  }
+])
+
+p "Created #{Message.count} msgs"
